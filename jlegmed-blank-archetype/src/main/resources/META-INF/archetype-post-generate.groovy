5d4016ff794daeb96b0c8ece28685a8c1f26e419
repplix <116941865+repplix@users.noreply.github.com>
2023-11-24 08:26:23 +0100

    Added workaround for windows systems together with Intellij: If the output-directory is not found under ArtifactID, we use the directory that exists

--- jlegmed-blank-archetype/src/main/resources/META-INF/archetype-post-generate.groovy
+++ jlegmed-blank-archetype/src/main/resources/META-INF/archetype-post-generate.groovy
@@ -2,6 +2,11 @@ void renameDir() {
     println "Executing the archetype-post-generate.groovy script..."
 
     def rootDir = new File(request.getOutputDirectory() + "/" + request.getArtifactId())
+    if (!rootDir.exists()) {
+        println "Root directory does not exist" + rootDir;
+        rootDir  = new File(request.getOutputDirectory() + "/" ).listFiles()[0];
+        println "New Root directory is" + rootDir;
+    }
     def projectDir = request.getOutputDirectory() + "/" + request.getProperties().getProperty("projectName")
     rootDir.renameTo(new File(projectDir))
 }
